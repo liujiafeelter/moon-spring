ceb54ca2c93f9dd6bd8936ece3ed93a6d85dc7a3
liujia02 <liujing123#*>
2020-02-29 21:25:26 +0800

    过滤器

--- spring-mvc/src/main/java/com/moon/spring/test/controller/UserController.java
+++ spring-mvc/src/main/java/com/moon/spring/test/controller/UserController.java
@@ -1,18 +1,16 @@
-package com.moon.spring.test;
+package com.moon.spring.test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
 
     @ResponseBody
     @RequestMapping("/getUser")
-    public String getUser(){
-        System.out.println("nihao");
+    public String getUser(String name){
+        System.out.println("nihao：" + name);
         return "nihao";
     }
 
