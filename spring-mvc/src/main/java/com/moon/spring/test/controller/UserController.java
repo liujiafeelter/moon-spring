324798f1f257f8445386cd5f6f4b59fd51b46c88
liujia02 <liujing123#*>
2020-03-09 20:56:47 +0800

    静态资源

--- spring-mvc/src/main/java/com/moon/spring/test/controller/UserController.java
+++ spring-mvc/src/main/java/com/moon/spring/test/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.moon.spring.test.controller;
 
 import org.springframework.stereotype.Controller;
+import com.moon.spring.test.domain.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class UserController {
 
     @ResponseBody
     @RequestMapping("/getUser")
-    public String getUser(String name){
+    public Object getUser(String name){
         System.out.println("nihao：" + name);
         return "nihao";
     }
+
+    @ResponseBody
+    @RequestMapping("/getUserJson")
+    public Object getUserJson(String name){
+        System.out.println("nihao：" + "getUserJson");
+        return new Response(1, "成功", "数据");
+    }
+
 
 }
---
+++ spring-mvc/src/main/webapp/WEB-INF/resources/js/commom.js
@@ -0,0 +1,5 @@
+
+function alertTest(){
+    alert("nihao");
+}
+
