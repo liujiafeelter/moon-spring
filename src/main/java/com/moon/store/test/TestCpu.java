fadf3ac6259f387988d79a4952f2c79aab1471a2
liujia741 <liujia741@jd.com>
2020-06-17 17:50:44 +0800

    mybatis

---
+++ src/main/java/com/moon/store/test/TestCpu.java
@@ -0,0 +1,14 @@
+package com.moon.store.test;
+
+public class TestCpu {
+
+    public static void main(String[] args) {
+        for (int i=0; ; i++){
+            if(i%10000 ==0) {
+                System.out.println("for:" + i);
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/com/moon/store/utils/TestRateLimiter.java
@@ -0,0 +1,53 @@
+package com.moon.store.utils;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TestRateLimiter {
+
+    static RateLimiter rateLimiter = RateLimiter.create(2.0);
+    static ThreadPoolExecutor executor = new ThreadPoolExecutor(50, 100,
+            100, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>());
+
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++){
+            submitTask(new Task(i));
+        }
+    }
+
+    public static void submitTask(Task task) {
+        rateLimiter.acquire();
+        executor.execute(task);
+    }
+
+}
+
+class Task implements Runnable{
+
+    int i;
+
+    public Task(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Task:" + i);
+        try {
+            Thread.sleep(10000);
+        }catch (Exception e){
+        }
+    }
+
+}
+
+
+
+
+
+
+
