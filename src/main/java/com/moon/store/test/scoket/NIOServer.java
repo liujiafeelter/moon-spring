afd69666a4361569511a2b79d0c3d353f6ebf0ac
liujia741 <liujia741@jd.com>
2020-07-31 19:21:26 +0800

    NIO

---
+++ src/main/java/com/moon/store/test/scoket/NIOServer.java
@@ -0,0 +1,25 @@
+package com.moon.store.test.scoket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * https://blog.csdn.net/u013857458/article/details/82424104
+ */
+public class NIOServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(8080));
+        serverSocketChannel.configureBlocking(false);
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+
+    }
+
+}
