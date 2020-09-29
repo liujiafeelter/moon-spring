a33e616c1288b9cf37846863f2cddae56fe7d011
liujiafeelter <liujialiujing925>
2020-09-29 14:42:33 +0800

    aop

---
+++ src/main/java/com/moon/store/aop/LogAopAspect2.java
@@ -0,0 +1,29 @@
+package com.moon.store.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
+public class LogAopAspect2 {
+
+    @Pointcut("execution(* com.moon..*.*(..))")
+    public void pointCut(){};
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint joinPoint) {
+        System.out.println("around2 start");
+        Object result = null;
+        try {
+            result = joinPoint.proceed(joinPoint.getArgs());
+        }catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        System.out.println("around2 end");
+        return result;
+    }
+
+}
