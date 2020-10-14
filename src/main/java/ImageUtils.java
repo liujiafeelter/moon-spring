5d010206876ddffc3f6eed4072204e9ce123b61c
liujia54 <liujia54@58.com>
2020-10-14 17:42:24 +0800

    '图片水印'

---
+++ src/main/java/ImageUtils.java
@@ -0,0 +1,105 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 水印中文乱码问题
+ * 服务器上没有相应的字体库
+ * linux：/usr/share/fonts
+ * windows：c:\windows\Fonts
+ *
+ * 查看服务器是否安装字体库: # fc-list   fc-list :lang=zh-cn
+ * 上传宋体，从windos上传即可  simsun.ttc
+ * 字体生效:fc-cache -fv
+ */
+public class ImageUtils {
+
+    private static final String PICTRUE_FORMATE_JPG = "jpg";
+
+    /**
+     * 添加文字水印
+     * @param targetImg 目标图片路径，如：C://myPictrue//1.jpg
+     * @param pressText 水印文字， 如：中国证券网
+     * @param fontName 字体名称，    如：宋体
+     * @param fontStyle 字体样式，如：粗体和斜体(Font.BOLD|Font.ITALIC)
+     * @param fontSize 字体大小，单位为像素
+     * @param color 字体颜色
+     * @param x 水印文字距离目标图片左侧的偏移量，如果x<0, 则在正中间
+     * @param y 水印文字距离目标图片上侧的偏移量，如果y<0, 则在正中间
+     * @param alpha 透明度(0.0 -- 1.0, 0.0为完全透明，1.0为完全不透明)
+     */
+    public static void pressText(String targetImg, String pressText, String fontName, int fontStyle, int fontSize, Color color, int x, int y, float alpha) {
+        try {
+            File file = new File(targetImg);
+            Image image = ImageIO.read(file);
+            int width = image.getWidth(null);
+            int height = image.getHeight(null);
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = bufferedImage.createGraphics();
+            g.drawImage(image, 0, 0, width, height, null);
+            g.setFont(new Font(fontName, fontStyle, fontSize));
+
+            //倾斜水印开始,图片也是倾斜的
+            g.rotate(Math.toRadians(-45),
+                    (double) bufferedImage.getWidth() / 2,
+                    (double) bufferedImage.getHeight() / 2);
+            //倾斜水印结束
+
+            g.setColor(color);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
+
+            int width_1 = fontSize * getLength(pressText);
+            int height_1 = fontSize;
+            int widthDiff = width - width_1;
+            int heightDiff = height - height_1;
+            if(x < 0){
+                x = widthDiff / 2;
+            }else if(x > widthDiff){
+                x = widthDiff;
+            }
+            if(y < 0){
+                y = heightDiff / 2;
+            }else if(y > heightDiff){
+                y = heightDiff;
+            }
+
+            //   g.drawString(pressText, x, y + height_1);
+            g.drawString(pressText, (int) (x*1.5), (y + height_1)/3);
+            //           g.drawString(pressText, (int) (x*1.5), (y + height_1)/2);
+            //      g.drawString(pressText, (int) (x*1.5), (int) ((y + height_1)*1.5));
+            //     g.drawString(pressText, x/2, (int) ((y + height_1)*1.5));
+
+
+            g.dispose();
+            ImageIO.write(bufferedImage, PICTRUE_FORMATE_JPG, file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取字符长度，一个汉字作为 1 个字符, 一个英文字母作为 0.5 个字符
+     * @param text
+     * @return 字符长度，如：text="中国",返回 2；text="test",返回 2；text="中国ABC",返回 4.
+     */
+    public static int getLength(String text) {
+        int textLength = text.length();
+        int length = textLength;
+        for (int i = 0; i < textLength; i++) {
+            if (String.valueOf(text.charAt(i)).getBytes().length > 1) {
+                length++;
+            }
+        }
+        return (length % 2 == 0) ? length / 2 : length / 2 + 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        pressText("aaabbb.jpg", "58.com 房本审核专用", "宋体",
+                Font.BOLD | Font.ITALIC, 120, Color.RED, -1, -1, 0.62f);
+        System.out.println("给图片添加水印文字结束...");
+    }
+
+
+}
--- src/main/java/com/moon/store/utils/DESUtil.java
+++ src/main/java/com/moon/store/utils/DESUtil.java
@@ -6,13 +6,15 @@ import sun.misc.BASE64Encoder;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import java.io.IOException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
 public class DESUtil {
 
-    private static String key = "123456";//私钥
+    private static String KEY = "949ad645e270d53d802d35a7be5c7886";//私钥
 
     /**
      * 加密
@@ -20,7 +22,7 @@ public class DESUtil {
      * @return
      * @throws Exception
      */
-    public static String encrypt(String content) throws Exception{
+    public static String encrypt(String content, String key) throws Exception{
         KeyGenerator kgen = KeyGenerator.getInstance("AES");
         SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
         random.setSeed(key.getBytes());
@@ -41,7 +43,7 @@ public class DESUtil {
      * @return
      * @throws Exception
      */
-    public static String decrypt(String content) throws Exception{
+    public static String decrypt(String content, String key) throws Exception{
         KeyGenerator kgen = KeyGenerator.getInstance("AES");
         SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
         random.setSeed(key.getBytes());
@@ -54,10 +56,40 @@ public class DESUtil {
 
         return new String(decryptResult,"utf-8");
     }
+    //=================================================================================================================
+
+    public static String encrypt2(String content, String key) throws Exception{
+        SecureRandom e = new SecureRandom();
+        DESKeySpec desKey = new DESKeySpec(key.getBytes());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey securekey = keyFactory.generateSecret(desKey);
+        Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, e);
+        byte[] encryptResult = cipher.doFinal(content.getBytes("utf-8"));
+        String result = new BASE64Encoder().encode(encryptResult);
+        return result;
+    }
+
+    public static String decrypt2(String content, String key) throws Exception{
+        SecureRandom random = new SecureRandom();
+        DESKeySpec desKey = new DESKeySpec(key.getBytes());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey securekey = keyFactory.generateSecret(desKey);
+        Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.DECRYPT_MODE, securekey, random);
+        byte[] decryptResult = cipher.doFinal(new BASE64Decoder().decodeBuffer(content));
+        return new String(decryptResult,"utf-8");
+    }
+
 
     public static void main(String[] args)throws Exception{
-        System.out.println(encrypt("你好你好你好"));
-        System.out.println(decrypt(encrypt("你好你好你好")));
+        System.out.println(encrypt("你好你好你好", KEY));
+        System.out.println(decrypt(encrypt("你好你好你好", KEY), KEY));
+
+        System.out.println("=======================");
+
+        System.out.println(encrypt2("你好你好你好", KEY));
+        System.out.println(decrypt2(encrypt2("你好你好你好", KEY), KEY));
     }
 
 }
